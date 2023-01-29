41035993201ca6e42332dc2d1251a3aaaeef8796
Rene M Chandler <qrencha@gmail.com>
2023-01-28 22:59:32 -0600

    Create lines_10PRINT.js

---
+++ lines/lines_10PRINT.js
@@ -0,0 +1,26 @@
+let dim = 600;
+let p0;
+let col = 20; // number of columns and rows
+let row = col;
+
+function setup() {
+    createCanvas(dim, dim);
+    strokeWeight(5);
+    frameRate(1);
+}
+
+function draw() {
+    background('darkgray');
+
+    for (let x = 0; x < col; x++) {
+        for (let y = 0; y < row; y++) {
+            if (random(1) < random([0.7, 0.3])) {
+                stroke(random(50,200),0,0);
+                line(x * dim / col, (y + 1) * dim / col, (x + 1) * dim / col, (y) * dim / col);
+            } else {
+                line(x * dim / col, (y) * dim / col, (x + 1) * dim / col, (y + 1) * dim / col);
+            }
+        }
+    }
+
+}
